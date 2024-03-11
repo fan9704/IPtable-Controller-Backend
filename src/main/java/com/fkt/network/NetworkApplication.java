1825af1716f77750cfd8dd80ab33453f91c14b74
fan9704 <cxz123499@gmail.com>
2024-03-11 11:08:20 +0800

    Init Commit

---
+++ src/main/java/com/fkt/network/NetworkApplication.java
@@ -0,0 +1,13 @@
+package com.fkt.network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkApplication.class, args);
+	}
+
+}
