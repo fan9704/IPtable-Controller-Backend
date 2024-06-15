7eb6facf515b054b8268b45e76ff5e13f867c608
fan9704 <cxz123499@gmail.com>
2024-06-15 14:18:38 +0800

    :sparkles: feat(rabbitmq) Client service

---
+++ src/main/java/com/fkt/network/configs/RabbitMQConfig.java
@@ -0,0 +1,43 @@
+package com.fkt.network.configs;
+
+
+import com.fkt.network.services.NATQueueClientService;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ConditionalOnExpression("${spring.rabbitmq.enable}")
+public class RabbitMQConfig {
+
+    @Value("${fkt.rabbitmq.receive.queue}")
+    String queueName;
+
+    @Value("${spring.rabbitmq.username}")
+    String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    //create MessageListenerContainer using default connection factory
+    @Bean
+    MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory ) {
+        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+        simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
+        simpleMessageListenerContainer.setQueues(queue());
+        simpleMessageListenerContainer.setMessageListener(new NATQueueClientService());
+        return simpleMessageListenerContainer;
+
+    }
+}
