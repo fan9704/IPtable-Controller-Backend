046f408572dda5e7ca5f4b213f1328217ebdde20
fan9704 <cxz123499@gmail.com>
2024-06-26 09:53:17 +0800

    :art: refactor(configs): RabbitMQConfig Listener Style Remove from dimension from Config and autoDelete Queue

--- src/main/java/com/fkt/network/configs/RabbitMQConfig.java
+++ src/main/java/com/fkt/network/configs/RabbitMQConfig.java
@@ -1,12 +1,10 @@
 package com.fkt.network.configs;
 
 
-import com.fkt.network.services.NATQueueClientService;
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,28 +17,24 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnExpression("${spring.rabbitmq.enable}")
 public class RabbitMQConfig {
 
-    @Value("${fkt.rabbitmq.receive.queue}")
-    String queueName;
+    String queueName = "nat.client";
 
     @Value("${spring.rabbitmq.username}")
     String username;
 
     @Value("${spring.rabbitmq.password}")
     private String password;
-    @Value("${rabbitmq.exchange.name:client}")
-    private String exchange;
+    private String exchange="client";
 
-    @Value("${rabbitmq.routing.json.key:140.96.83.14}")
-    private String routingJsonKey;
+    private String routingJsonKey ="client";
     @Bean
     Queue queue() {
-        return new Queue(this.queueName, false);
+        return new Queue(this.queueName, false,false,true);
     }
     @Bean
     public TopicExchange exchange(){
         return new TopicExchange(this.exchange);
     }
-    //TODO:Auto Bind
     @Bean
     public Binding jsonBinding(){
         return BindingBuilder
@@ -48,24 +42,8 @@ public class RabbitMQConfig {
                 .to(exchange())
                 .with(routingJsonKey);
     }
-    //create MessageListenerContainer using default connection factory
-    @Bean
-    MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory ) {
-        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
-        simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
-        simpleMessageListenerContainer.setQueues(queue());
-        simpleMessageListenerContainer.setMessageListener(new NATQueueClientService());
-        return simpleMessageListenerContainer;
-
-    }
     @Bean
     public MessageConverter converter(){
         return new Jackson2JsonMessageConverter();
-    }
-    @Bean
-    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory){
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(converter());
-        return rabbitTemplate;
     }
 }
